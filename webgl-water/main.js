fef3e5ad4858a6d05eea6bd69c5cbe5b61bf02bc
czhowl <czhhzc94@gmail.com>
2019-05-30 18:12:31 -0400

    add another water

---
+++ webgl-water/main.js
@@ -0,0 +1,249 @@
+/*
+ * WebGL Water
+ * http://madebyevan.com/webgl-water/
+ *
+ * Copyright 2011 Evan Wallace
+ * Released under the MIT license
+ */
+
+function text2html(text) {
+  return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\n/g, '<br>');
+}
+
+function handleError(text) {
+  var html = text2html(text);
+  if (html == 'WebGL not supported') {
+    html = 'Your browser does not support WebGL.<br>Please see\
+    <a href="http://www.khronos.org/webgl/wiki/Getting_a_WebGL_Implementation">\
+    Getting a WebGL Implementation</a>.';
+  }
+  var loading = document.getElementById('loading');
+  loading.innerHTML = html;
+  loading.style.zIndex = 1;
+}
+
+window.onerror = handleError;
+
+var gl = GL.create();
+var water;
+var cubemap;
+var renderer;
+var angleX = -90;
+var angleY = -0;
+
+// Sphere physics info
+var useSpherePhysics = false;
+var center;
+var oldCenter;
+var velocity;
+var gravity;
+var radius;
+var paused = false;
+
+window.onload = function() {
+  var ratio = window.devicePixelRatio || 1;
+  var help = document.getElementById('help');
+
+  function onresize() {
+    var width = innerWidth - help.clientWidth - 20;
+    var height = innerHeight;
+    gl.canvas.width = width * ratio;
+    gl.canvas.height = height * ratio;
+    gl.canvas.style.width = width + 'px';
+    gl.canvas.style.height = height + 'px';
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.matrixMode(gl.PROJECTION);
+    gl.loadIdentity();
+    gl.perspective(45, gl.canvas.width / gl.canvas.height, 0.01, 100);
+    gl.matrixMode(gl.MODELVIEW);
+    draw();
+  }
+
+  document.body.appendChild(gl.canvas);
+  gl.clearColor(0, 0, 0, 1);
+
+  water = new Water();
+  renderer = new Renderer();
+  cubemap = new Cubemap({
+    xneg: document.getElementById('xneg'),
+    xpos: document.getElementById('xpos'),
+    yneg: document.getElementById('ypos'),
+    ypos: document.getElementById('ypos'),
+    zneg: document.getElementById('zneg'),
+    zpos: document.getElementById('zpos')
+  });
+
+  if (!water.textureA.canDrawTo() || !water.textureB.canDrawTo()) {
+    throw new Error('Rendering to floating-point textures is required but not supported');
+  }
+
+  center = oldCenter = new GL.Vector(-0.4, -0.75, 0.2);
+  velocity = new GL.Vector();
+  gravity = new GL.Vector(0, -4, 0);
+  radius = 0.25;
+
+  for (var i = 0; i < 20; i++) {
+    water.addDrop(Math.random() * 2 - 1, Math.random() * 2 - 1, 0.03, (i & 1) ? 0.01 : -0.01);
+  }
+
+  document.getElementById('loading').innerHTML = '';
+  onresize();
+
+  var requestAnimationFrame =
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    function(callback) { setTimeout(callback, 0); };
+
+  var prevTime = new Date().getTime();
+  function animate() {
+    var nextTime = new Date().getTime();
+    if (!paused) {
+      update((nextTime - prevTime) / 1000);
+      draw();
+    }
+    prevTime = nextTime;
+    requestAnimationFrame(animate);
+  }
+  requestAnimationFrame(animate);
+
+  window.onresize = onresize;
+
+  var prevHit;
+  var planeNormal;
+  var mode = -1;
+  var MODE_ADD_DROPS = 0;
+  var MODE_ORBIT_CAMERA = 1;
+
+  var oldX, oldY;
+
+  function startDrag(x, y, e) {
+    oldX = x;
+    oldY = y;
+    var tracer = new GL.Raytracer();
+    var ray = tracer.getRayForPixel(x * ratio, y * ratio);
+    var pointOnPlane = tracer.eye.add(ray.multiply(-tracer.eye.y / ray.y));
+    var sphereHitTest = GL.Raytracer.hitTestSphere(tracer.eye, ray, center, radius);
+    if (!e) {
+      mode = MODE_ADD_DROPS;
+      duringDrag(x, y);
+    } else {
+      mode = MODE_ORBIT_CAMERA;
+    }
+  }
+
+  function duringDrag(x, y) {
+    switch (mode) {
+      case MODE_ADD_DROPS: {
+        var tracer = new GL.Raytracer();
+        var ray = tracer.getRayForPixel(x * ratio, y * ratio);
+        console.log(x * ratio + "   " + y);
+        var pointOnPlane = tracer.eye.add(ray.multiply(-tracer.eye.y / ray.y));
+        water.addDrop(pointOnPlane.x, pointOnPlane.z, 0.03, 0.01);
+        if (paused) {
+          water.updateNormals();
+          renderer.updateCaustics(water);
+        }
+        break;
+      }
+      case MODE_ORBIT_CAMERA: {
+        angleY -= x - oldX;
+        angleX -= y - oldY;
+        //angleX = Math.max(-89.999, Math.min(89.999, angleX));
+        break;
+      }
+    }
+    oldX = x;
+    oldY = y;
+    if (paused) draw();
+  }
+
+  function stopDrag() {
+    mode = -1;
+  }
+
+  function isHelpElement(element) {
+    return element === help || element.parentNode && isHelpElement(element.parentNode);
+  }
+
+  document.onmousedown = function(e) {
+    // if (!isHelpElement(e.target)) {
+      e.preventDefault();
+      startDrag(e.pageX, e.pageY, isHelpElement(e.target));
+    // }
+  };
+
+  document.onmousemove = function(e) {
+    duringDrag(e.pageX, e.pageY);
+  };
+
+  document.onmouseup = function() {
+    stopDrag();
+  };
+
+  document.ontouchstart = function(e) {
+    if (e.touches.length === 1 && !isHelpElement(e.target)) {
+      e.preventDefault();
+      startDrag(e.touches[0].pageX, e.touches[0].pageY, e.touches.length === 1 && !isHelpElement(e.target));
+    }
+  };
+
+  document.ontouchmove = function(e) {
+    if (e.touches.length === 1) {
+      duringDrag(e.touches[0].pageX, e.touches[0].pageY);
+    }
+  };
+
+  document.ontouchend = function(e) {
+    if (e.touches.length == 0) {
+      stopDrag();
+    }
+  };
+
+  document.onkeydown = function(e) {
+    if (e.which == ' '.charCodeAt(0)) paused = !paused;
+    else if (e.which == 'L'.charCodeAt(0) && paused) draw();
+  };
+
+  var frame = 0;
+
+  function update(seconds) {
+    if (seconds > 1) return;
+    frame += seconds * 2;
+
+    // Displace water around the sphere
+    oldCenter = center;
+
+    // Update the water simulation and graphics
+    water.stepSimulation();
+    water.stepSimulation();
+    water.updateNormals();
+    renderer.updateCaustics(water);
+  }
+
+  function draw() {
+    // Change the light direction to the camera look vector when the L key is pressed
+    //if (GL.keys.L) {
+      renderer.lightDir = GL.Vector.fromAngles((90 - angleY) * Math.PI / 180, -angleX * Math.PI / 180);
+      if (paused) renderer.updateCaustics(water);
+    //}
+
+    // angleX = -90;
+    // angleY = 0;
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.loadIdentity();
+    gl.translate(0, 0, -4);
+    gl.rotate(90, 1, 0, 0);
+    gl.rotate(0, 0, 1, 0);
+    gl.translate(0, 0.1, 0);
+    
+
+    gl.enable(gl.DEPTH_TEST);
+    renderer.sphereCenter = center;
+    renderer.sphereRadius = radius;
+    //renderer.renderCube();
+    renderer.renderWater(water, cubemap);
+    //renderer.renderSphere();
+    gl.disable(gl.DEPTH_TEST);
+  }
+};
---
+++ webgl-water/renderer.js
@@ -0,0 +1,225 @@
+/*
+ * WebGL Water
+ * http://madebyevan.com/webgl-water/
+ *
+ * Copyright 2011 Evan Wallace
+ * Released under the MIT license
+ */
+
+var helperFunctions = '\
+  const float IOR_AIR = 1.0;\
+  const float IOR_WATER = 1.333;\
+  \
+  const vec3 underwaterColor = vec3(0.4, 0.4, 1.0);\
+  uniform vec3 light;\
+  uniform sampler2D tiles;\
+  uniform sampler2D causticTex;\
+  uniform sampler2D water;\
+  uniform float poolHeight;\
+  \
+  vec2 intersectCube(vec3 origin, vec3 ray, vec3 cubeMin, vec3 cubeMax) {\
+    vec3 tMin = (cubeMin - origin) / ray;\
+    vec3 tMax = (cubeMax - origin) / ray;\
+    vec3 t1 = min(tMin, tMax);\
+    vec3 t2 = max(tMin, tMax);\
+    float tNear = max(max(t1.x, t1.y), t1.z);\
+    float tFar = min(min(t2.x, t2.y), t2.z);\
+    return vec2(tNear, tFar);\
+  }\
+  \
+  vec3 getWallColor(vec3 point) {\
+    float scale = 0.5;\
+    \
+    vec3 wallColor;\
+    vec3 normal;\
+      wallColor = texture2D(tiles, point.xz * 0.1 + 0.5).rgb;\
+      normal = vec3(0.0, 1.0, 0.0);\
+    \
+    scale /= length(point); /* pool ambient occlusion */\
+    \
+    /* caustics */\
+    vec3 refractedLight = -refract(-light, vec3(0.0, 1.0, 0.0), IOR_AIR / IOR_WATER);\
+    float diffuse = max(0.0, dot(refractedLight, normal));\
+    vec4 info = texture2D(water, point.xz * 0.5 + 0.5);\
+    \
+      vec4 caustic = texture2D(causticTex, 0.75 * (point.xz - point.y * refractedLight.xz / refractedLight.y) * 0.25 + 0.5);\
+      scale += diffuse * caustic.r * 2.0 * caustic.g;\
+      \
+      scale += diffuse * 0.5;\
+    \
+    return wallColor * scale;\
+  }\
+';
+
+function Renderer() {
+  this.tileTexture = GL.Texture.fromImage(document.getElementById('tiles'), {
+    // minFilter: gl.LINEAR_MIPMAP_LINEAR,
+    // wrap: gl.CLAMP_TO_EDGE,
+    format: gl.RGB
+  });
+  this.lightDir = new GL.Vector(2.0, 2.0, -1.0).unit();
+  this.causticTex = new GL.Texture(1024, 1024);
+  this.waterMesh = GL.Mesh.plane({ detail: 200 });
+  this.waterShaders = [];
+  for (var i = 0; i < 2; i++) {
+    this.waterShaders[i] = new GL.Shader('\
+      uniform sampler2D water;\
+      varying vec3 position;\
+      void main() {\
+        vec4 info = texture2D(water, gl_Vertex.xy * 0.5 + 0.5);\
+        position = gl_Vertex.xzy;\
+        position.y += info.r;\
+        position.x *= 5.0;\
+        position.z *= 2.0;\
+        gl_Position = gl_ModelViewProjectionMatrix * vec4(position, 1.0);\
+      }\
+    ', helperFunctions + '\
+      uniform vec3 eye;\
+      varying vec3 position;\
+      uniform samplerCube sky;\
+      \
+      vec3 getSurfaceRayColor(vec3 origin, vec3 ray, vec3 waterColor) {\
+        vec3 color;\
+        float q = 1.0e6;\
+        if (ray.y < 0.0) {\
+          vec2 t = intersectCube(origin, ray, vec3(-5.0, -poolHeight, -2.0), vec3(5.0, 2.0, 2.0));\
+          color = getWallColor(origin + ray * t.y);\
+        } else {\
+          vec2 t = intersectCube(origin, ray, vec3(-5.0, -poolHeight, -2.0), vec3(5.0, 2.0, 2.0));\
+          vec3 hit = origin + ray * t.y;\
+          if (hit.y < 2.0 / 12.0) {\
+            color = getWallColor(hit);\
+          } else {\
+            color = textureCube(sky, ray).rgb;\
+            color += vec3(pow(max(0.0, dot(light, ray)), 5000.0)) * vec3(10.0, 8.0, 6.0);\
+          }\
+        }\
+        if (ray.y < 0.0) color *= waterColor;\
+        return color;\
+      }\
+      \
+      void main() {\
+        vec2 coord = position.xz;\
+        coord.x += 2.5;\
+        coord.y += 2.5;\
+        coord.x *= 0.2;\
+        coord.y *= 0.2;\
+        vec4 info = texture2D(water, coord);\
+        \
+        /* make water look more "peaked" */\
+        for (int i = 0; i < 5; i++) {\
+          coord += info.ba * 0.005;\
+          info = texture2D(water, coord);\
+        }\
+        \
+        vec3 normal = vec3(info.b, sqrt(1.0 - dot(info.ba, info.ba)), info.a);\
+        vec3 incomingRay = normalize(position - eye);\
+        \
+        ' + /* above water */ '\
+          vec3 reflectedRay = reflect(incomingRay, normal);\
+          vec3 refractedRay = refract(incomingRay, normal, IOR_AIR / IOR_WATER);\
+          float fresnel = mix(0.25, 1.0, pow(1.0 - dot(normal, -incomingRay), 3.0));\
+          \
+          vec3 abovewaterColor = vec3(0.3 - position.z);\
+          vec3 reflectedColor = getSurfaceRayColor(position, reflectedRay, abovewaterColor);\
+          vec3 refractedColor = getSurfaceRayColor(position, refractedRay, abovewaterColor);\
+          \
+          gl_FragColor = vec4(mix(refractedColor, reflectedColor, fresnel), 1.0);\
+        ' + '\
+      }\
+    ');
+  }
+  var hasDerivatives = !!gl.getExtension('OES_standard_derivatives');
+  this.causticsShader = new GL.Shader(helperFunctions + '\
+    varying vec3 oldPos;\
+    varying vec3 newPos;\
+    varying vec3 ray;\
+    \
+    /* project the ray onto the plane */\
+    vec3 project(vec3 origin, vec3 ray, vec3 refractedLight) {\
+      vec2 tcube = intersectCube(origin, ray, vec3(-1.0, -poolHeight, -1.0), vec3(1.0, 2.0, 1.0));\
+      origin += ray * tcube.y;\
+      float tplane = (-origin.y - 1.0) / refractedLight.y;\
+      return origin + refractedLight * tplane;\
+    }\
+    \
+    void main() {\
+      vec4 info = texture2D(water, gl_Vertex.xy * 0.5 + 0.5);\
+      info.ba *= 0.5;\
+      vec3 normal = vec3(info.b, sqrt(1.0 - dot(info.ba, info.ba)), info.a);\
+      \
+      /* project the vertices along the refracted vertex ray */\
+      vec3 refractedLight = refract(-light, vec3(0.0, 1.0, 0.0), IOR_AIR / IOR_WATER);\
+      ray = refract(-light, normal, IOR_AIR / IOR_WATER);\
+      oldPos = project(gl_Vertex.xzy, refractedLight, refractedLight);\
+      newPos = project(gl_Vertex.xzy + vec3(0.0, info.r, 0.0), ray, refractedLight);\
+      \
+      gl_Position = vec4(1.0 * (newPos.xz + refractedLight.xz / refractedLight.y), 0.0, 1.0);\
+    }\
+  ', (hasDerivatives ? '#extension GL_OES_standard_derivatives : enable\n' : '') + '\
+    ' + helperFunctions + '\
+    varying vec3 oldPos;\
+    varying vec3 newPos;\
+    varying vec3 ray;\
+    \
+    void main() {\
+      ' + (hasDerivatives ? '\
+        /* if the triangle gets smaller, it gets brighter, and vice versa */\
+        float oldArea = length(dFdx(oldPos)) * length(dFdy(oldPos));\
+        float newArea = length(dFdx(newPos)) * length(dFdy(newPos));\
+        gl_FragColor = vec4(oldArea / newArea * 0.2, 1.0, 0.0, 0.0);\
+      ' : '\
+        gl_FragColor = vec4(0.2, 0.2, 0.0, 0.0);\
+      ' ) + '\
+      \
+      vec3 refractedLight = refract(-light, vec3(0.0, 1.0, 0.0), IOR_AIR / IOR_WATER);\
+      \
+      /* compute a blob shadow and make sure we only draw a shadow if the player is blocking the light */\
+      vec3 dir = newPos;\
+      vec3 area = cross(dir, refractedLight);\
+      float shadow = dot(area, area);\
+      float dist = dot(dir, -refractedLight);\
+      shadow = 1.0 + (shadow - 1.0) / (0.05 + dist * 0.025);\
+      shadow = clamp(1.0 / (1.0 + exp(-shadow)), 0.0, 1.0);\
+      shadow = mix(1.0, shadow, clamp(dist * 2.0, 0.0, 1.0));\
+      gl_FragColor.g = shadow;\
+      \
+    }\
+  ');
+}
+
+Renderer.prototype.updateCaustics = function(water) {
+  if (!this.causticsShader) return;
+  var this_ = this;
+  this.causticTex.drawTo(function() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    water.textureA.bind(0);
+    this_.causticsShader.uniforms({
+      light: this_.lightDir,
+      water: 0,
+      poolHeight: 0.5
+    }).draw(this_.waterMesh);
+  });
+};
+
+Renderer.prototype.renderWater = function(water, sky) {
+  var tracer = new GL.Raytracer();
+  water.textureA.bind(0);
+  this.tileTexture.bind(1);
+  sky.bind(2);
+  this.causticTex.bind(3);
+  gl.enable(gl.CULL_FACE);
+  for (var i = 0; i < 2; i++) {
+    gl.cullFace(i ? gl.BACK : gl.FRONT);
+    this.waterShaders[i].uniforms({
+      light: this.lightDir,
+      water: 0,
+      tiles: 1,
+      sky: 2,
+      causticTex: 3,
+      eye: tracer.eye,
+      poolHeight: 0.5
+    }).draw(this.waterMesh);
+  }
+  gl.disable(gl.CULL_FACE);
+};
